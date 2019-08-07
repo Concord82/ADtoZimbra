0f03b7a2c603827f7a69d39b1b7bf9c5667b39fa
lviv <viv@cons.tsk.ru>
2019-08-07 17:37:31 +0700

    Обновленно

--- configtest.py
+++ configtest.py
@@ -80,7 +80,51 @@ class Config:
 		with open(os.path.splitext(path)[0] + '.tmp', "w") as config_file:
 			config.write(config_file)
 
-def main():
+
+# процедура возвращающая словарь со списком листов рассылки и адресов чденов группы
+# словарь имеет вид:
+#	ключь - адрес списка рассылки: [ список адресов почты членов шруппы ] 
+def get_ad_grouplist():
+	server = Server(conf.ad_server, conf.ad_port, conf.ad_tls)
+	try:
+		connection = Connection(server, user=conf.ad_user, password=conf.ad_password, authentication=NTLM, auto_bind=True)
+	except LDAPSocketOpenError as err:
+		logger.critical('Ошибка соединения с сервером. Проверьте адрес сервера в файле конфигурации.')
+		logger.critical(err)
+		sys.exit(1)
+	except LDAPBindError as err:
+		logger.critical('Ошибка соединения с сервером. Неверный логин или пароль.')
+		logger.critical(err)
+		sys.exit(1)
+	
+	connection.search(search_base=conf.ad_search_base, 
+	search_filter='(objectClass=group)', search_scope=SUBTREE, attributes = ['cn', 'distinguishedName', 'mail','member'])
+	# создаем словарь для наполнения его элементами
+	ad_group_dict = {}
+	# перебираем полученные группы из заданного OU
+	for entries in connection.entries:
+		# список адресов электронной почты членов группы
+		# создаем или обнуляем при следующей итерации цикла
+		member_email = []
+		# перебираем членов группы в цикле
+		for member in entries.member.values:
+			# ищем адрес почты члена группы
+			connection.search(
+				search_base='OU=CONSULTANT,dc=cons,dc=tsk,dc=ru',
+				search_filter=f'(distinguishedName={member})',
+				attributes = ['mail']
+			)
+			# вносим полученный адрес в созданный временный список
+			member_email.append(connection.entries[0].mail.values)
+		# после перебора добавляем к словарю значение
+		# ключь адрес почты группы
+		# значение временный список
+		ad_group_dict[entries.mail.value] = member_email
+
+	return ad_group_dict
+
+
+def create_logger():
 	"""
 	Based on http://docs.python.org/howto/logging.html#configuring-logging
 	"""
@@ -138,24 +182,11 @@ if __name__ == '__main__':
 		print ('от имени которого запускается скрипт на возможность записи в указанный каталог ')
 		sys.exit(1)
 	
-	logger = main()
-	server = Server(conf.ad_server, conf.ad_port, conf.ad_tls)
-	try:
-		connection = Connection(server, user=conf.ad_user, password=conf.ad_password, authentication=NTLM, auto_bind=True)
-	except LDAPSocketOpenError as err:
-		logger.critical('Ошибка соединения с сервером. Проверьте адрес сервера в файле конфигурации.')
-		logger.critical(err)
-		sys.exit(1)
-	except LDAPBindError as err:
-		logger.critical('Ошибка соединения с сервером. Неверный логин или пароль.')
-		logger.critical(err)
-		sys.exit(1)
-	
-	connection.search(search_base=conf.ad_search_base, 
-	search_filter='(objectClass=group)', search_scope=SUBTREE, attributes = ['cn', 'distinguishedName', 'mail','member'])
-	
+	logger = create_logger()
+
+	ad_group_dict = get_ad_grouplist()
 	# список групп рассылки из AD
-	ad_address_list = []
+	ad_address_list = [k for k in ad_group_dict.keys()]
 	for entries in connection.entries:
 		ad_address_list.append(entries.mail.values[0])
 	
@@ -179,11 +210,17 @@ if __name__ == '__main__':
 		logger.info('Удаляется список рассылки: %s ', address_for_del)
 		proc = subprocess.Popen([conf.zmprov, 'ddl', address_for_del], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		stdout = proc.communicate()[0]
+		if stdout == b'':
+			logger.info('Выполненно успешно')
 		
-		print (stdout)
-		
+	for ad_addres_item in ad_address_list:
+		if not ad_addres_item in zimbra_address_list:
+			logger.info('Список рассылки %s не найден', ad_addres_item)
+			logger.info('выполняем добавление')
+			out = check_output([conf.zmprov, ])
 	
-		
+	
+	print (ad_group_dict)
 	
 	
 	
