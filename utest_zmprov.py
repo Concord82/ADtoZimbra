daaa656597fd31d784f7c2571cd26ed1fb07d7db
root <admin-cj@cons.tsk.ru>
2019-08-09 18:02:50 +0700

    zmprov utest added

--- utest_zmprov.py
+++ utest_zmprov.py
@@ -32,24 +32,35 @@ class ZimbraClientTest(unittest.TestCase):
 		''' Список доменов обслуживаемых Zimbra'''
 		print("id: " + self.id())
 		getalldomain = self.zm_exec.getalldomain()
-		self.assertEqual(getalldomain, ['zimbra-mailserver.internal.cons.tsk.ru', 'test.cons.tsk.ru'])
+		self.assertEqual(
+			getalldomain, 
+			['zimbra-mailserver.internal.cons.tsk.ru', 'test.cons.tsk.ru']
+		)
 		
 	def test_02_getAlldistributionLists(self):
 		''' Список листов рассылки на сервере'''
 		print("id: " + self.id())
 		AllDistribList = self.zm_exec.getAlldistributionLists()
 		print (AllDistribList)
-		self.assertIn('cons-talks@zimbra-mailserver.internal.cons.tsk.ru', AllDistribList)
+		self.assertIn(
+			'cons-talks@zimbra-mailserver.internal.cons.tsk.ru', 
+			AllDistribList
+		)
 		
 	def test_03_createDistributionList(self):
 		''' Создаем список рассылки'''
 		print("id: " + self.id())
-		self.assertNotEqual(self.zm_exec.createDistributionList('test2@test.cons.tsk.ru'), False)
+		self.assertNotEqual(
+			self.zm_exec.createDistributionList('test2@test.cons.tsk.ru'), 
+			False
+		)
 		
 	def test_04_createDistributionList(self):
 		''' Создаем список рассылки повторно для проверки обработчика ошибок'''
 		print("id: " + self.id())
-		self.assertFalse(self.zm_exec.createDistributionList('test2@test.cons.tsk.ru'))
+		self.assertFalse(
+			self.zm_exec.createDistributionList('test2@test.cons.tsk.ru')
+		)
 		
 	def test_05_addDistributionListMember(self):
 		'''добавляем 10 членов в список рассылки '''
@@ -57,12 +68,19 @@ class ZimbraClientTest(unittest.TestCase):
 		user_list = []
 		for i in range(10):
 			user_list.append('user'+str(i)+'@test.cons.tsk.ru')
-		self.assertTrue( self.zm_exec.addDistributionListMember('cons-talks@zimbra-mailserver.internal.cons.tsk.ru', user_list) )
+		self.assertTrue( 
+			self.zm_exec.addDistributionListMember(
+				'cons-talks@zimbra-mailserver.internal.cons.tsk.ru', 
+				user_list
+			)
+		)
 			
 	def test_06_getDistributionListmembership(self):
 		'''получаем членов списка рассылки '''
 		print("id: " + self.id())
-		DistributionListmembership = self.zm_exec.getDistributionListmembership('cons-talks@zimbra-mailserver.internal.cons.tsk.ru')
+		DistributionListmembership = self.zm_exec.getDistributionListmembership(
+			'cons-talks@zimbra-mailserver.internal.cons.tsk.ru'
+		)
 		flagg = True
 		for i in range(10):
 			if not 'user'+str(i)+'@test.cons.tsk.ru' in DistributionListmembership:
@@ -73,7 +91,11 @@ class ZimbraClientTest(unittest.TestCase):
 	def test_07_getDistributionListmembership(self):
 		'''получаем членов несуществующего списка рассылки '''
 		print("id: " + self.id())
-		self.assertFalse( self.zm_exec.getDistributionListmembership('cons-talks434@zimbra-mailserver.internal.cons.tsk.ru'))
+		self.assertFalse( 
+			self.zm_exec.getDistributionListmembership(
+				'cons-talks434@zimbra-mailserver.internal.cons.tsk.ru'
+			)
+		)
 		
 	def test_08_removeDistributionListMember(self):
 		'''удаляем 10 членов из списка рассылки '''
@@ -81,7 +103,12 @@ class ZimbraClientTest(unittest.TestCase):
 		user_list = []
 		for i in range(10):
 			user_list.append('user'+str(i)+'@test.cons.tsk.ru')
-		self.assertTrue( self.zm_exec.removeDistributionListMember('cons-talks@zimbra-mailserver.internal.cons.tsk.ru', user_list) )
+		self.assertTrue( 
+			self.zm_exec.removeDistributionListMember(
+				'cons-talks@zimbra-mailserver.internal.cons.tsk.ru', 
+				user_list
+			)
+		)
 		
 	def test_09_addDistributionListMember(self):
 		'''добавляем 100 членов в список рассылки '''
@@ -89,7 +116,12 @@ class ZimbraClientTest(unittest.TestCase):
 		user_list = []
 		for i in range(100):
 			user_list.append('user'+str(i)+'@test.cons.tsk.ru')
-		self.assertTrue( self.zm_exec.addDistributionListMember('cons-talks@zimbra-mailserver.internal.cons.tsk.ru', user_list) )
+		self.assertTrue( 
+			self.zm_exec.addDistributionListMember(
+				'cons-talks@zimbra-mailserver.internal.cons.tsk.ru', 
+				user_list
+			)
+		)
 		
 	def test_10_removeDistributionListMember(self):
 		'''удаляем 100 членов из списка рассылки '''
@@ -97,7 +129,12 @@ class ZimbraClientTest(unittest.TestCase):
 		user_list = []
 		for i in range(10):
 			user_list.append('user'+str(i)+'@test.cons.tsk.ru')
-		self.assertTrue( self.zm_exec.removeDistributionListMember('cons-talks@zimbra-mailserver.internal.cons.tsk.ru', user_list) )
+		self.assertTrue( 
+			self.zm_exec.removeDistributionListMember(
+				'cons-talks@zimbra-mailserver.internal.cons.tsk.ru', 
+				user_list
+			)
+		)
 		
 	def test_11_addDistributionListMember(self):
 		'''добавляем члена в несуществующий список рассылки '''
@@ -122,12 +159,20 @@ class ZimbraClientTest(unittest.TestCase):
 	def test_13_deleteDistributionList(self):
 		''' Удаляем список рассылки'''
 		print("id: " + self.id())
-		self.assertTrue(self.zm_exec.deleteDistributionList('test2@test.cons.tsk.ru'))
+		self.assertTrue(
+			self.zm_exec.deleteDistributionList(
+				'test2@test.cons.tsk.ru'
+			)
+		)
 		
 	def test_14_deleteDistributionList(self):
 		''' Удаляем список рассылки повторно'''
 		print("id: " + self.id())
-		self.assertFalse(self.zm_exec.deleteDistributionList('test2@test.cons.tsk.ru'))
+		self.assertFalse(
+			self.zm_exec.deleteDistributionList(
+				'test2@test.cons.tsk.ru'
+			)
+		)
 		
 if __name__ == '__main__':
    unittest.main()
